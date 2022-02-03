fbeb74ab4918130beae780ce8773f628e41b8794
Tom Aarsen <Cubiegamedev@gmail.com>
2022-02-03 23:11:32 +0100

    Add some simple Scanner tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,27 @@
-
 from typing import List
 import pytest
+from glob import glob
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 @pytest.fixture(scope="session")
-def bool_program() -> List[str]:
+def bool_program() -> str:
     with open("data/given/valid/bool.spl", "r", encoding="utf8") as f:
         return f.read()
+
+
+def files() -> List[str]:
+    return [
+        file
+        for file in glob("data/given/**/*.spl", recursive=True)
+        if "list_ops" not in file
+    ]
+
+
+@pytest.fixture(scope="session", params=files())
+def file(request) -> str:
+    return request.param
--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -1,25 +1,50 @@
-
-from typing import List
 from compiler import Scanner, Token, Type
+
+from tests.test_util import open_file
+
 
 def test_scan(bool_program: str):
     scanner = Scanner()
     tokens = scanner.scan(bool_program)
-    
+
     expected = [
         Token("xor", Type.ID, 1),
         Token("(", Type.LRB, 1),
         Token("a", Type.ID, 1),
         Token(",", Type.COMMA, 1),
-        Token("b", Type.ID, 1)
+        Token("b", Type.ID, 1),
     ]
 
     assert tokens[:5] == expected
+
 
 def test_empty():
     scanner = Scanner()
     tokens = scanner.scan("")
     assert tokens == []
 
-# aa, a0, 00 should work
-# 0a should error
+
+def test_remove_comments(file: str):
+    program: str = open_file(file)
+    scanner = Scanner()
+
+    modified = scanner.remove_comments(program)
+    # Ensure that the number of lines remains the same
+    assert len(program.splitlines()) == len(modified.splitlines())
+    # Ensure that this method only reduces length, or changes nothing
+    assert len(modified) <= len(program)
+
+    remodified = scanner.remove_comments(modified)
+    # Ensure that reusing `remove_comments` makes no further changes
+    assert modified == remodified
+
+
+def test_scan(file: str):
+    program: str = open_file(file)
+    scanner = Scanner()
+
+    tokens = scanner.scan(program)
+    # Ensure that we get a non-empty list of tokens,
+    # unless the program itself is empty or just whitespace.
+    # Specifically, ensure no crashing.
+    assert len(tokens) > 1 or not program.strip()
