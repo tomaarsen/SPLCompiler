01cba0eafec77e5c076235337f55f4ac717fb4f8
MahieB <51122015+MahieB@users.noreply.github.com>
2022-02-04 12:52:02 +0100

    Combined multiple consecutive UnexpectedCharacterErrors into a single error

--- compiler/errors.py
+++ compiler/errors.py
@@ -23,16 +23,35 @@ class CompilerError:
     @staticmethod
     def __combine_errors():
         # Check if we have any consecutive UnexpectedCharacterError
-        for i, error in enumerate(CompilerError.ERRORS[::]):
-            if (i < len(CompilerError.ERRORS)-1):
-                next_error = CompilerError.ERRORS[i+1]
+        # First sort on line_no, and then on start of the error (span[0]) in line
+        # If error object has no span attribute, then sort it on top
+        CompilerError.ERRORS.sort(
+            key=lambda error: (error.line_no, error.span[0])
+            if hasattr(error, "span")
+            else (error.line_no, -1)
+        )
 
-                if isinstance(error, UnexpectedCharacterError) and isinstance(next_error, UnexpectedCharacterError):
-
-                    if error.span[1] == next_error.span[0]:
-                        # CompilerError.ERRORS.remove(next_error)
-                        CompilerError.ERRORS[i+1] = UnexpectedCharacterError(error.line, error.line_no, (error.span[0], next_error.span[1]))
-
+        length = len(CompilerError.ERRORS)
+        i = 0
+        while i < length - 1:
+            if (
+                isinstance(CompilerError.ERRORS[i], UnexpectedCharacterError)
+                and isinstance(CompilerError.ERRORS[i + 1], UnexpectedCharacterError)
+                and CompilerError.ERRORS[i].span[1]
+                == CompilerError.ERRORS[i + 1].span[0]
+            ):
+                CompilerError.ERRORS[i + 1] = UnexpectedCharacterError(
+                    CompilerError.ERRORS[i].line,
+                    CompilerError.ERRORS[i].line_no,
+                    (
+                        CompilerError.ERRORS[i].span[0],
+                        CompilerError.ERRORS[i + 1].span[1],
+                    ),
+                )
+                del CompilerError.ERRORS[i]
+                length -= 1
+                i -= 1
+            i += 1
 
     @staticmethod
     def raise_all():
@@ -42,7 +61,8 @@ class CompilerError:
         if errors:
             sys.tracebacklimit = -1
             if len(CompilerError.ERRORS) > 10:
-                errors += f"\n\nShowing 10 errors, omitting {len(CompilerError.ERRORS)-10} error(s)..."
+                omitting_multiple_errors = len(CompilerError.ERRORS) - 10 > 1
+                errors += f"\n\nShowing 10 errors, omitting {len(CompilerError.ERRORS)-10} error{'s' if omitting_multiple_errors else ''}..."
             CompilerError.ERRORS.clear()
             raise Exception(errors)
 
@@ -59,8 +79,9 @@ class UnexpectedCharacterError(QueueableError):
     span: Tuple[int, int]
 
     def __str__(self) -> str:
+        multiple_unexpected_chars = (self.span[1] - self.span[0]) > 1
         return (
-            f"Unknown character {self.line[self.span[0]:self.span[1]]!r} on line {self.line_no}.\n"
+            f"Unexpected character{'s' if multiple_unexpected_chars else ''} {self.line[self.span[0]:self.span[1]]!r} on line {self.line_no}.\n"
             f"  {self.line_no}. {self.line[:self.span[0]]}{Colors.RED}{self.line[self.span[0]:self.span[1]]}{Colors.ENDC}{self.line[self.span[1]:]}"
         )
 
@@ -76,6 +97,7 @@ class MissingSemicolonError(QueueableError):
             f"  {self.line_no}. {self.line}"
         )
 
+
 @dataclass
 class UnmatchableTokenError(QueueableError):
     line: str
@@ -86,6 +108,7 @@ class UnmatchableTokenError(QueueableError):
             f"Unexpected lack of token match on line: {self.line_no}.\n"
             f"  {self.line_no}. {self.line}"
         )
+
 
 @dataclass
 class DanglingMultiLineCommentError(QueueableError):
@@ -99,6 +122,7 @@ class DanglingMultiLineCommentError(QueueableError):
             f"  {self.line_no}. {self.line[:self.match.start()]}{Colors.RED}{self.line[self.match.start():self.match.end()]}{Colors.ENDC}{self.line[self.match.end():]}"
         )
 
+
 @dataclass
 class LonelyQuoteError(QueueableError):
     line: str
@@ -110,6 +134,7 @@ class LonelyQuoteError(QueueableError):
             f"Found lonely quote on line: {self.line_no}.\n"
             f"  {self.line_no}. {self.line[:self.match.start()]}{Colors.RED}{self.line[self.match.start():self.match.end()]}{Colors.ENDC}{self.line[self.match.end():]}"
         )
+
 
 if __name__ == "__main__":
     # Example usage:
