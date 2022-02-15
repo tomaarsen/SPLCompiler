2716a606019939fb54078cd92d30599c210051aa
Mathieu Bangma <m.bangma@student.science.ru.nl>
2022-02-15 10:00:16 +0100

    Comply with pre-commit

--- demo.py
+++ demo.py
@@ -1,9 +1,10 @@
 from pprint import pprint
-from compiler import Scanner, Parser
+
+from compiler import Parser, Scanner
 
 with open("data/given/valid/bool.spl", "r", encoding="utf8") as f:
-# with open("data/bracketed.spl", "r", encoding="utf8") as f:
-# with open("data/given/invalid/unbalanced_parenthesis2.spl", "r", encoding="utf8") as f:
+    # with open("data/bracketed.spl", "r", encoding="utf8") as f:
+    # with open("data/given/invalid/unbalanced_parenthesis2.spl", "r", encoding="utf8") as f:
     program = f.read()
 
 program = """
